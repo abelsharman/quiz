1a3cec81cb150786685b3cb60d422c0041f6b3ed
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2022-06-19 02:35:58 +0600

    new: vue project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Quiz</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+    margin: 0;
+    font-family: 'Lato', sans-serif;
+}
+h1, h2, h3, h4, h5, p, strong {
+    margin: 0;
+    padding: 0;
+}
+</style>
